4f04b370d0d7314a511bf498df3eb2d27a5e868e
Talisa Erazo <talisaerazo@gmail.com>
2018-10-16 01:32:05 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["cupcakes", "cake", "cookies", "candy"]
+  array = ["cupcakes", "cake", "cookies", "candy"]
 end
 
 def add_element_to_end_of_array(array, element)
